c0845382dc236d100c8b4cd458bfce63dd23eee2
Henry Borchers <henryborchers@yahoo.com>
2019-06-07 12:51:29 -0500

    use the steps to bridge

--- src/org/ds/python/ToxRunner.groovy
+++ src/org/ds/python/ToxRunner.groovy
@@ -5,46 +5,44 @@ class ToxRunner implements Serializable {
     ToxRunner(steps){this.steps = steps}
 
     def run_tox_test_in_node(pythonToolName, pythonPkgFile, test_args, nodeLabels){
-        script{
-            def stashCode = UUID.randomUUID().toString()
-            stash includes: "${pythonPkgFile}", name: "${stashCode}"
+        def stashCode = UUID.randomUUID().toString()
+        steps.stash includes: "${pythonPkgFile}", name: "${stashCode}"
 
-            node("${nodeLabels}"){
-                def python_home = "${tool pythonToolName}"
-                def python_exec = '"' + python_home + "\\python.exe" + '"'
+        steps.node("${nodeLabels}"){
+            def python_home = "${steps.tool pythonToolName}"
+            def python_exec = '"' + python_home + "\\python.exe" + '"'
 
-                def python_version = bat(
-                        label: "Checking Python version for ${python_exec}",
-                        returnStdout: true,
-                        script: "@${python_exec} --version").trim()
+            def python_version = steps.bat(
+                    label: "Checking Python version for ${python_exec}",
+                    returnStdout: true,
+                    script: "@${python_exec} --version").trim()
 
-                if(python_version == ""){
-                    error("No Python version detected")
+            if(python_version == ""){
+                steps.error("No Python version detected")
+            }
+            try{
+                steps.checkout scm
+                steps.withEnv(['VENVPATH=venv']) {
+                    steps.bat(label: "Create virtualenv based on ${python_version} on ${NODE_NAME} node",
+                            script: "${python_exec} -m venv %VENVPATH%"
+                    )
+                    steps.bat(label: "Update pip version in virtualenv",
+                            script: "%VENVPATH%\\Scripts\\python.exe -m pip install pip --upgrade"
+                    )
+
+
+                    steps.bat(label: "Install Tox in virtualenv",
+                            script: "%VENVPATH%\\Scripts\\pip install tox"
+                    )
+
+                    steps.unstash "${stashCode}"
+                    steps.bat(label: "Testing ${pythonPkgFile}",
+                            script: "%VENVPATH%\\Scripts\\tox.exe -c ${WORKSPACE}/tox.ini --parallel=auto -o --workdir=${WORKSPACE}/tox --installpkg=${pythonPkgFile} ${test_args} -vv"
+                    )
                 }
-                try{
-                    checkout scm
-                    withEnv(['VENVPATH=venv']) {
-                        bat(label: "Create virtualenv based on ${python_version} on ${NODE_NAME} node",
-                                script: "${python_exec} -m venv %VENVPATH%"
-                        )
-                        bat(label: "Update pip version in virtualenv",
-                                script: "%VENVPATH%\\Scripts\\python.exe -m pip install pip --upgrade"
-                        )
-
-
-                        bat(label: "Install Tox in virtualenv",
-                                script: "%VENVPATH%\\Scripts\\pip install tox"
-                        )
-
-                        unstash "${stashCode}"
-                        bat(label: "Testing ${pythonPkgFile}",
-                                script: "%VENVPATH%\\Scripts\\tox.exe -c ${WORKSPACE}/tox.ini --parallel=auto -o --workdir=${WORKSPACE}/tox --installpkg=${pythonPkgFile} ${test_args} -vv"
-                        )
-                    }
-                }
-                finally{
-                    deleteDir()
-                }
+            }
+            finally{
+                steps.deleteDir()
             }
         }
     }
