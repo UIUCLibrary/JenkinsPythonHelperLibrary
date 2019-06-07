c96f34601b2155e05b8c7e681f049d52cd9bf8e9
Henry Borchers <henryborchers@yahoo.com>
2019-06-07 12:30:01 -0500

    runner in ToxRunner class

--- vars/testPythonPackage.groovy
+++ vars/testPythonPackage.groovy
@@ -1,3 +1,5 @@
+import org.ds.python.ToxRunner
+
 def call(Map args){
     def defaultArgs = [
             testNodeLabels: "Python3",
@@ -17,51 +19,53 @@ def call(Map args){
     def testEnvironments = environments.join(" ")
 
     pythonPkgs.each{
-        run_tox_test_in_node(args.pythonToolName, it, testEnvironments, args.testNodeLabels)
+        def runner = new ToxRunner()
+        runner.run_tox_test_in_node(args.pythonToolName, it, testEnvironments, args.testNodeLabels)
+//        run_tox_test_in_node(args.pythonToolName, it, testEnvironments, args.testNodeLabels)
     }
 }
-
-def run_tox_test_in_node(pythonToolName, pythonPkgFile, test_args, nodeLabels){
-    script{
-        def stashCode = UUID.randomUUID().toString()
-        stash includes: "${pythonPkgFile}", name: "${stashCode}"
-
-        node("${nodeLabels}"){
-            def python_home = "${tool pythonToolName}"
-            def python_exec = '"' + python_home + "\\python.exe" + '"'
-
-            def python_version = bat(
-                    label: "Checking Python version for ${python_exec}",
-                    returnStdout: true,
-                    script: "@${python_exec} --version").trim()
-
-            if(python_version == ""){
-                error("No Python version detected")
-            }
-            try{
-                checkout scm
-                withEnv(['VENVPATH=venv']) {
-                    bat(label: "Create virtualenv based on ${python_version} on ${NODE_NAME}",
-                            script: "${python_exec} -m venv %VENVPATH%"
-                    )
-                    bat(label: "Update pip version in virtualenv",
-                            script: "%VENVPATH%\\Scripts\\python.exe -m pip install pip --upgrade"
-                    )
-
-
-                    bat(label: "Install Tox in virtualenv",
-                            script: "%VENVPATH%\\Scripts\\pip install tox"
-                    )
-
-                    unstash "${stashCode}"
-                    bat(label: "Testing ${pythonPkgFile}",
-                            script: "%VENVPATH%\\Scripts\\tox.exe -c ${WORKSPACE}/tox.ini --parallel=auto -o --workdir=${WORKSPACE}/tox --installpkg=${pythonPkgFile} ${test_args} -vv"
-                    )
-                }
-            }
-            finally{
-                deleteDir()
-            }
-        }
-    }
-}
+//
+//def run_tox_test_in_node(pythonToolName, pythonPkgFile, test_args, nodeLabels){
+//    script{
+//        def stashCode = UUID.randomUUID().toString()
+//        stash includes: "${pythonPkgFile}", name: "${stashCode}"
+//
+//        node("${nodeLabels}"){
+//            def python_home = "${tool pythonToolName}"
+//            def python_exec = '"' + python_home + "\\python.exe" + '"'
+//
+//            def python_version = bat(
+//                    label: "Checking Python version for ${python_exec}",
+//                    returnStdout: true,
+//                    script: "@${python_exec} --version").trim()
+//
+//            if(python_version == ""){
+//                error("No Python version detected")
+//            }
+//            try{
+//                checkout scm
+//                withEnv(['VENVPATH=venv']) {
+//                    bat(label: "Create virtualenv based on ${python_version} on ${NODE_NAME} node",
+//                            script: "${python_exec} -m venv %VENVPATH%"
+//                    )
+//                    bat(label: "Update pip version in virtualenv",
+//                            script: "%VENVPATH%\\Scripts\\python.exe -m pip install pip --upgrade"
+//                    )
+//
+//
+//                    bat(label: "Install Tox in virtualenv",
+//                            script: "%VENVPATH%\\Scripts\\pip install tox"
+//                    )
+//
+//                    unstash "${stashCode}"
+//                    bat(label: "Testing ${pythonPkgFile}",
+//                            script: "%VENVPATH%\\Scripts\\tox.exe -c ${WORKSPACE}/tox.ini --parallel=auto -o --workdir=${WORKSPACE}/tox --installpkg=${pythonPkgFile} ${test_args} -vv"
+//                    )
+//                }
+//            }
+//            finally{
+//                deleteDir()
+//            }
+//        }
+//    }
+//}
